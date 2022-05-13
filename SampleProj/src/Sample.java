e5629989195e449339aefb3d5aa320cdac9b3b3b
AkshatKumarRai <100740416+AkshatKumarRai@users.noreply.github.com>
2022-05-13 07:54:21 +0530

    Pushing source code

---
+++ SampleProj/src/Sample.java
@@ -0,0 +1,13 @@
+
+public class Sample {
+	public static void main(String[] args) {
+		String str1 = "akshat is a good programmer";
+		String str2 = "";
+		for (int i = str1.length() - 1; i >= 0; --i) {
+			str2 = str2 + str1.charAt(i);
+		}
+		System.out.println("Original string:" + str1);
+		System.out.println("reverse string:" + str2);
+
+}
+}
